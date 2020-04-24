0c7a9a55ec0df1c0104c2c17f0b1caa2376618a3
Roger Hsu <datar-ai@users.noreply.github.com>
2020-04-24 14:28:10 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#bert-serving-start -num_worker=1 -model_dir /app/uncased_L-24_H-1024_A-16/
-bert-serving-start -num_worker=1 -model_dir /app/uncased_L-12_H-768_A-12/
+bert-serving-start -num_worker=1 -model_dir /app/uncased_L-24_H-1024_A-16/
+#bert-serving-start -num_worker=1 -model_dir /app/uncased_L-12_H-768_A-12/
