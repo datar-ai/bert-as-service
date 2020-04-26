8786e380b0a4d935b4a50bc7739b5d8acd1ebb95
Roger Hsu <datar-ai@users.noreply.github.com>
2020-04-26 23:55:09 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,11 @@ do
   echo "ENTRYPOINT arguments:" + $1
   if [ "$1" = 'bash' ]; then
     bash
+  elseif [ "$1" = 'chinese' ]; then
+    bert-serving-start -http_port 8001 -num_worker=1 -model_dir $1 $2
+  elseif [ "$1" = 'base' ]; then
+    bert-serving-start -http_port 8001 -num_worker=1 -model_dir $1 $2
   else
-    bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/uncased_L-12_H-768_A-12/ $1
+    bert-serving-start -http_port 8001 -num_worker=1 -model_dir $1 $2
   fi
 done
