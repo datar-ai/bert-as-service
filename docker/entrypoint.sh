c290dd2d677fa16d939688fa8d08a1a4c44f3f20
Roger Hsu <datar-ai@users.noreply.github.com>
2020-04-27 00:55:53 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,17 @@ set -x
 while $1
 do
   echo "ENTRYPOINT arguments:" + $1
-  if [ "$1" = "bash" ]; then
+  if [ "$1" = "bash" ]
+  then
     bash
-  elif [ "$1" = "chinese" ]; then
+  elif [ "$1" = "chinese" ]
+  then
     bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/chinese_L-12_H-768_A-12 $2
-  elif [ "$1" = "base" ]; then
+  elif [ "$1" = "base" ]
+  then
     bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/uncased_L-12_H-768_A-12 $2
-  elif [ "$1" = "large" ]; then
+  elif [ "$1" = "large" ]
+  then
     bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/uncased_L-24_H-1024_A-16 $2
   else
     bert-serving-start -http_port 8001 -num_worker=1 -model_dir $1 $2
