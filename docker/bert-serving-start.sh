567d607baa2e59be9549540c51c4288c3660ddc0
Roger Hsu <datar-ai@users.noreply.github.com>
2020-05-15 00:44:35 +0800

    Update bert-serving-start.sh

--- docker/bert-serving-start.sh
+++ docker/bert-serving-start.sh
@@ -6,13 +6,13 @@ then
   bash
 elif [ "$1" = "chinese" ]
 then
-  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/data/chinese_L-12_H-768_A-12 $2 $3 $4 $5 $6
+  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/chinese_L-12_H-768_A-12 $2 $3 $4 $5 $6
 elif [ "$1" = "base" ]
 then
-  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/data/uncased_L-12_H-768_A-12 $2 $3 $4 $5 $6
+  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/uncased_L-12_H-768_A-12 $2 $3 $4 $5 $6
 elif [ "$1" = "large" ]
 then
-  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/data/uncased_L-24_H-1024_A-16 $2 $3 $4 $5 $6
+  bert-serving-start -http_port 8001 -num_worker=1 -model_dir /app/uncased_L-24_H-1024_A-16 $2 $3 $4 $5 $6
 else
   bert-serving-start -http_port 8001 -num_worker=1 -model_dir $1 $2 $3 $4 $5 $6
 fi
